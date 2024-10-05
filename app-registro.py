94b2aa44bcb7083d01074bab87f0709ac7cd8459
jserrataylor <jose.serra@upr.edu>
2024-10-05 08:37:52 -0400

    Update app-registro.py

--- app-registro.py
+++ app-registro.py
@@ -14,12 +14,12 @@ c = conn.cursor()
 
 # Crear la tabla de usuarios si no existe
 c.execute("""
-CREATE TABLE IF NOT EXISTS usuarios (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre TEXT NOT NULL,
-    email TEXT NOT NULL,
-    asistencia INTEGER DEFAULT 0
-)
+    CREATE TABLE IF NOT EXISTS usuarios (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT NULL,
+        email TEXT NOT NULL,
+        asistencia INTEGER DEFAULT 0
+    )
 """)
 conn.commit()
 
@@ -121,7 +121,6 @@ else:
                     st.success('Datos exportados exitosamente.')
             else:
                 st.error('Credenciales de administrador incorrectas.')
-
 
 '''
 import streamlit as st

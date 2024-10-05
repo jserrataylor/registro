c0563ab349de8623b0c83a25f6e500795b27b5d5
jserrataylor <jose.serra@upr.edu>
2024-10-05 08:36:19 -0400

    Update app-registro.py

--- app-registro.py
+++ app-registro.py
@@ -13,12 +13,14 @@ conn = sqlite3.connect('usuarios.db')
 c = conn.cursor()
 
 # Crear la tabla de usuarios si no existe
-c.execute('''CREATE TABLE IF NOT EXISTS usuarios (
+c.execute("""
+CREATE TABLE IF NOT EXISTS usuarios (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre TEXT NOT NULL,
     email TEXT NOT NULL,
     asistencia INTEGER DEFAULT 0
-)''')
+)
+""")
 conn.commit()
 
 # Obtener los parámetros de la URL
